9adb8446efb2518036edc60f669b7c488987d597
elena-if3 <elena.rottin@gmail.com>
2025-07-30 15:21:50 +0200

    Event class skeleton

---
+++ models/event/event.py
@@ -0,0 +1,17 @@
+class Event:
+
+    @staticmethod
+    def festival() -> None:
+        pass
+
+    @staticmethod
+    def fire() -> None:
+        pass
+
+    @staticmethod
+    def flood() -> None:
+        pass
+
+    @staticmethod
+    def strike() -> None:
+        pass

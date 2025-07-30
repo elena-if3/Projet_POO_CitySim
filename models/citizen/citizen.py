0995ec0f2a74195c273187e024bf5edf742b23e7
elena-if3 <elena.rottin@gmail.com>
2025-07-30 14:25:05 +0200

    Citizen getters & setters

--- models/citizen/citizen.py
+++ models/citizen/citizen.py
@@ -9,4 +9,34 @@ class Citizen:
         self.__satisfaction = satisfaction
         self.__day_activity = day_activity
 
-        
+    @property
+    def name(self):
+        return self.__name
+    
+    @property
+    def age(self):
+        return self.__age
+    
+    @age.setter
+    def age(self, value: int):
+        self.__age = value
+    
+    @property
+    def profession(self):
+        return self.__profession
+    
+    @profession.setter
+    def profession(self, value : Profession):
+        self.__profession = value
+    
+    @property
+    def satisfaction(self):
+        return self.__satisfaction
+    
+    @satisfaction.setter
+    def satisfaction(self, value: int):
+        self.__satisfaction = value
+
+    @property
+    def day_activity(self):
+        return self.__day_activity

4070486c3a9f1e25ec45c95064a11221bb97e2ef
hartjoa <hartjoa@gmail.com>
2025-07-30 13:47:11 +0200

    added Facility's properties

--- models/facility/facility.py
+++ models/facility/facility.py
@@ -5,18 +5,30 @@ class Facility:
     This abstract class serves as base class for all facilities (production, housing and leisure)
     """
     def __init__(self, name, capacity, integrity = 100):
-        self.name = name
-        self.capacity = capacity
-        self.integrity = integrity
+        self.__name = name
+        self.__capacity = capacity
+        self.__integrity = integrity
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def capacity(self):
+        return self.__capacity
+
+    @property
+    def integrity(self):
+        return self.__integrity
 
     def repair(self, reparation: int = 10):
-        if self.integrity + reparation > 100:
-            self.integrity = 100
+        if self.__integrity + reparation > 100:
+            self.__integrity = 100
         else:
-            self.integrity += reparation
+            self.__integrity += reparation
 
     def damage(self, damage: int = 10):
-        if self.integrity - damage < 0:
-            self.integrity = 0
+        if self.__integrity - damage < 0:
+            self.__integrity = 0
         else:
-            self.integrity -= damage
+            self.__integrity -= damage

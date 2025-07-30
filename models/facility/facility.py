b14a3cda2f09a61a40f4f2393741e8e009b5e0b7
hartjoa <hartjoa@gmail.com>
2025-07-30 13:40:43 +0200

    Implemented Facility class

--- models/facility/facility.py
+++ models/facility/facility.py
@@ -1,4 +1,22 @@
 from abc import ABC, abstractmethod
 
 class Facility:
-    pass
+    """
+    This abstract class serves as base class for all facilities (production, housing and leisure)
+    """
+    def __init__(self, name, capacity, integrity = 100):
+        self.name = name
+        self.capacity = capacity
+        self.integrity = integrity
+
+    def repair(self, reparation: int = 10):
+        if self.integrity + reparation > 100:
+            self.integrity = 100
+        else:
+            self.integrity += reparation
+
+    def damage(self, damage: int = 10):
+        if self.integrity - damage < 0:
+            self.integrity = 0
+        else:
+            self.integrity -= damage

cabf9456c18d74a0cecb9e87a5c8b8c6bf984880
Sacha Senas <sachasenas@gmail.com>
2025-07-30 19:32:05 +0200

    remove default dict from init, replace issubclass by isinstance for objects, dict now return default value if key is not init, fixed list comprehension error

--- Models/City.py
+++ Models/City.py
@@ -1,12 +1,12 @@
 import random
 
 class City:
-    def __init__(self, name="Springfield", facilities={}, resources={}):
+    def __init__(self, name="Springfield", facilities=None, resources=None):
         self.__day = 0
         self.__daily_log = []
         self.__name = name
-        self.__facilities = facilities
-        self.__resources = resources
+        self.__facilities = facilities or {}
+        self.__resources = resources or {}
         self.__homeless_citizens = []
 
     @property
@@ -35,7 +35,7 @@ class City:
 
     @property
     def capacity(self):
-        return sum(housing.capacity for housing in self.__facilities[Housing])
+        return sum(housing.capacity for housing in self.__facilities.get(Housing, []))
 
     @property
     def weekday(self):
@@ -43,35 +43,37 @@ class City:
 
     @property
     def citizens(self):
-        citizens = [inhabitant for inhabitant in housing.inhabitants for housing in self.__facilities[Housing]]
-        citizens.extend(self.__homeless_citizens)
-        return citizens
+        return [inhabitant for housing in self.__facilities.get(Housing, []) for inhabitant in housing.inhabitants] + self.__homeless_citizens
 
     @property
     def happiness(self):
-        return sum(citizen.satisfaction for citizen in self.__citizens) // len(self.__citizens)
+        citizens = self.citizens
+        if citizens:
+            return sum(citizen.satisfaction for citizen in citizens) // len(citizens)
+        return 0
 
     def add_facility(self, facility):
-        if not issubclass(type(facility), Facility):
-            raise TypeError("Not a subclass of Facility")
-        self.__facilities[type(facility)].append(facility)
+        if not isinstance(facility, Facility):
+            raise TypeError("Not a Facility instance")
+        self.__facilities.setdefault(type(facility), []).append(facility)
 
     def add_citizen(self, citizen):
         if not isinstance(citizen, Citizen):
-            raise TypeError("Not an instance of citizen")
-        housing_available = [housing for housing in self.__facilities[Housing] if housing.capacity > len(housing.inhabitants)]
+            raise TypeError("Not a Citizen instance")
+        housing_available = [housing for housing in self.__facilities.get(Housing, []) if housing.capacity > len(housing.inhabitants)]
         if housing_available:
             random.choice(housing_available).add_inhabitant(citizen)
         else:
             self.__homeless_citizens.append(citizen)
 
     def add_resource(self, resource):
-        if not issubclass(type(resource), Resource):
-            raise TypeError("Not a subclass of Resource")
-        self.__resources[type(resource)] += self.__resources.get(type(resource), 0) + resource.amount
+        if not isinstance(resource, Resource):
+            raise TypeError("Not a Resource instance")
+        self.__resources[type(resource)] = self.__resources.get(type(resource), 0) + resource.amount
 
     def live_day(self):
-        production_facilities = [facility for facility in self.__facilities if issubclass(type(facility), Production)]
-        for production_facility in production_facilities:
-            resource = production_facility.produce()
-            self.add_resource(resource)
+        for facility_type, facilities in self.__facilities.items():
+            if issubclass(facility_type, Production):
+                for facility in facilities:
+                    self.add_resource(facility.produce())
+        self.__day += 1

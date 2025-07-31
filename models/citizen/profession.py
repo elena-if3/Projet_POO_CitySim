24a85f208f3639f1261034f530a4c8b39d56ee14
elena-if3 <elena.rottin@gmail.com>
2025-07-31 13:36:03 +0200

    Profession updated + Work class added

--- models/citizen/profession.py
+++ models/citizen/profession.py
@@ -4,6 +4,7 @@ from models.facility.power_plant import PowerPlant
 from models.facility.water_supply_plant import WaterSupplyPlant
 
 
+
 class Profession(Enum):
     FOOD_FACTORY = FoodFactory
     POWER_PLANT = PowerPlant

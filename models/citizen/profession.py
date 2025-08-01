064a4eb1faafef0b33e2275f30d63fe255bf2c79
Kaenali0n <147976780+Kaenali0n@users.noreply.github.com>
2025-08-01 11:16:27 +0200

    Update profession.py
    Add import for objects

--- models/citizen/profession.py
+++ models/citizen/profession.py
@@ -1,8 +1,11 @@
 from enum import Enum
+from models.facility.food_factory import FoodFactory
+from models.facility.power_plant import PowerPlant
+from models.facility.water_supply_plant import WaterSupplyPlant
 
 
 class Profession(Enum):
     FOOD_FACTORY = FoodFactory
     POWER_PLANT = PowerPlant
-    WATER_FACILITY = WaterFacility
+    WATER_SUPPLY_PLANT = WaterSupplyPlant
     JOBLESS = jobless

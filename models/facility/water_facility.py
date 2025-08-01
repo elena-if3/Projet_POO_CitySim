c434b138fa581f50a467f01d46bd304501cdc3ce
hartjoa <42883820+hartjoa@users.noreply.github.com>
2025-08-01 13:41:10 +0200

    Facility > main (#12)
    * add tools folder and constants module, implement produce()
    
    * Complete typing. Adapt produce() methods to handle day and day shift. Add is_full property to all Facility children. Add users list to Leisure.
    
    * Add add_worker() to Production
    
    * Fix typo
    
    * Decouple functionalities from production.get_working_workers(): the execution of worker.work() is moved onto another method while this one only returns the list of working workers.
    
    * Rename Factory classes
    
    * Add damage resulting of factory production.
    
    * implement grow() for all failities classes
    
    * Use absolute paths everywhere.  Add default values for facilities arguments. Add leisure.add_user().  Adapt damage in leisure. Add test file.
    
    * Add tests. Check worker's profession when adding to factory. Fix kwargs issue.

--- models/facility/water_facility.py
+++
@@ -1,9 +0,0 @@
-from .production import Production
-from ..resource.water import Water
-
-class WaterFacility(Production):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-    
-    def produce(self):
-        return (Water, len(self.workers))
--- models/facility/water_supply_plant.py
+++ models/facility/water_supply_plant.py
@@ -1,22 +1,22 @@
 from models.facility.factory import Factory
-from models.resource.electricity import Electricity
+from models.resource.water import Water
 from tools.constants import *
 
-class PowerPlant(Factory):
-    def __init__(self, name = "Power Plant", **kwargs):
+class WaterSupplyPlant(Factory):
+    def __init__(self, name = "Water Supply Plant", **kwargs):
         super().__init__(name = name, **kwargs)
     
-    def produce(self, day: int, is_day_shift: bool) -> "Electricity":
+    def produce(self, day: int, is_day_shift: bool) -> "Water":
         # Get factory workers working on that shift
         working_workers = super().get_working_workers(day, is_night)
         # Make them work
         super.workers_work(working_workers)
         # Degrade factory
-        super().damage(ELECTRICITY_PRODUCTION_DAMAGE)
+        super().damage(WATER_PRODUCTION_DAMAGE)
         # Get total quantity produced during that shift
-        produced_qty = super().daily_production(ELECTRICITY_DAILY_PRODUCTION_PER_WORKER, len(working_workers))
-        return Electricity(produced_qty)
+        produced_qty = super().daily_production(WATER_DAILY_PRODUCTION_PER_WORKER, len(working_workers))
+        return Water(produced_qty)
     
     def grow(self, electricity: "Electricity", water: "Water", is_night: bool) -> None:
-        water.amount -= WATER_LOSS_DAILY_POWER_PLANT
+        electricity.amount -= ELECTRICITY_LOSS_DAILY_POWER_PLANT
         super().grow()

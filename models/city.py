d60bf0f0f8eb71b7afe6e5bb0835826a5e372ef3
SachitoGH <134655474+SachitoGH@users.noreply.github.com>
2025-08-01 14:17:17 +0200

    City (#12)
    * added city.py
    
    * empty list for city constrcutor, rename var, corrected add_resource logic
    
    * change attribute from public to private and added getter
    
    * remove default dict from init, replace issubclass by isinstance for objects, dict now return default value if key is not init, fixed list comprehension error
    
    * rename folder/file
    
    * first version of live day
    
    * add import
    
    * add live_night first version
    
    * change is_day to is_night
    
    * remove bad import
    
    * add live_complete_day to combine live_day and live_night
    
    * rename factory
    
    * add water to facility grow call
    
    * add is_night to grow city
    
    * use is_full for housing
    
    * delete daily log
    
    * absolute path and change init default value logic
    
    ---------
    
    Co-authored-by: Sacha Senas <sachasenas@gmail.com>

--- models/city.py
+++ models/city.py
@@ -1,12 +1,26 @@
 import random
+from models.facility.facility import Facility
+from models.facility.housing import Housing
+from models.facility.leisure import Leisure
+from models.facility.factory import Factory
+from models.resource.resource import Resource
+from models.resource.food import Food
+from models.resource.water import Water
+from models.resource.electricity import Electricity
+from models.citizen.citizen import Citizen
 
 class City:
     def __init__(self, name="Springfield", facilities=None, resources=None):
         self.__day = 0
-        self.__daily_log = []
         self.__name = name
-        self.__facilities = facilities or {}
-        self.__resources = resources or {}
+        if facilities is None:
+            self.__facilities = {}
+        else:
+            self.__facilities = facilities
+        if resources is None:
+            self.__resources = {}
+        else:
+            self.__resources = resources
         self.__homeless_citizens = []
 
     @property
@@ -16,10 +30,6 @@ class City:
     @property
     def name(self):
         return self.__name
-
-    @property
-    def daily_log(self):
-        return self.__daily_log
 
     @property
     def facilities(self):
@@ -57,23 +67,96 @@ class City:
             raise TypeError("Not a Facility instance")
         self.__facilities.setdefault(type(facility), []).append(facility)
 
-    def add_citizen(self, citizen):
+    def add_citizen_housing(self, citizen):
         if not isinstance(citizen, Citizen):
             raise TypeError("Not a Citizen instance")
-        housing_available = [housing for housing in self.__facilities.get(Housing, []) if housing.capacity > len(housing.inhabitants)]
+        housing_available = [housing for housing in self.__facilities.get(Housing, []) if not housing.is_full]
         if housing_available:
             random.choice(housing_available).add_inhabitant(citizen)
+            return True
         else:
             self.__homeless_citizens.append(citizen)
+            return False
 
     def add_resource(self, resource):
         if not isinstance(resource, Resource):
             raise TypeError("Not a Resource instance")
         self.__resources[type(resource)] = self.__resources.get(type(resource), 0) + resource.amount
 
-    def live_day(self):
+    def factory_produce(self, is_night):
         for facility_type, facilities in self.__facilities.items():
-            if issubclass(facility_type, Production):
+            if issubclass(facility_type, Factory):
                 for facility in facilities:
-                    self.add_resource(facility.produce())
+                    self.add_resource(facility.produce(weekday=self.weekday, is_night=is_night))
+
+    def event_happen(self):
+        pass
+
+    def update_citizens_facilities(self):
+        for facility_type, facilities in self.facilities.items():
+            for facility in facilities:
+                if facility.integrity <= 0:
+                    if isinstance(facility, Housing):
+                        self.__homeless_citizens.append(facility.inhabitants)
+                    self.__facilities[facility_type].remove(facility)
+        for homeless_citizen in self.__homeless_citizens:
+            if not homeless_citizen.is_alive:
+                self.__homeless_citizens.remove(homeless_citizen)
+
+    def attribute_factory(self, citizen):
+        for factories in self.__facilities[citizen.profession].values():
+            for factory in factories:
+                if not factory.is_full:
+                    factory.add_worker(citizen)
+                    return True
+        return False
+
+    def municipality(self):
+        # repair facilities
+        for facilities in self.__facilities.values():
+            for facility in facilities:
+                if facility.integrity <= 70:
+                    facility.repair(25)
+        # reattribute homeless
+        for homeless_citizen in self.__homeless_citizens:
+            if not self.add_citizen_housing(homeless_citizen):
+                break
+        # reattribute work
+        for citizen in self.citizens:
+            if not citizen.work.employed:
+                self.attribute_factory(citizen)
+
+    def citizens_leisure(self):
+        for citizen in self.citizens:
+            if self.weekday in citizen.work.off_days:
+                citizen.leisure(self.__facilities[Leisure])
+
+    def grow_citizen(self, is_night):
+        for citizen in self.citizens:
+            citizen.grow(food=self.__resources[Food], water=self.__resources[Water], is_night=is_night)
+
+    def grow_city(self, is_night):
+        for facilities in self.__facilities.values():
+            for facility in facilities:
+                facility.grow(electricity=self.__resources[Electricity], water=self.__resources[Water], is_night=is_night)
+        self.grow_citizen(is_night=is_night)
+
+    def live_day(self):
+        self.event_happen()
+        self.update_citizens_facilities()
+        self.municipality()
+        self.factory_produce(is_night=False)
+        self.citizens_leisure()
+        self.grow_city(is_night=False)
+        self.update_citizens_facilities()
+
+    def live_night(self):
+        self.event_happen()
+        self.update_citizens_facilities()
+        self.factory_produce(is_night=True)
+        self.grow_city(is_night=True)
+
+    def live_complete_day(self):
+        self.live_day()
+        self.live_night()
         self.__day += 1
